aa8cd961ac74a1e3b99a9f96336f11d56d9c173c
ashishsecrets <ashishsecrets@users.noreply.github.com>
2021-04-14 18:55:03 +0530

    Restored ApiError and pushing credentials.json for server access to google sheets

--- src/main/java/com/ucsf/payload/response/ApiError.java
+++ src/main/java/com/ucsf/payload/response/ApiError.java
@@ -1,28 +1,27 @@
 package com.ucsf.payload.response;
 
 public class ApiError {
-	
-	private int code;
-	private String message;
-	
-	 public ApiError(int code, String message) {
-		    this.code = code;
-		    this.message = message;
-		  }
 
-	 
-		  public String getDescription() {
-		     return message;
-		  }
+	public int code;
+	public String message;
 
-		  public int getCode() {
-		     return code;
-		  }
+	public ApiError(int code, String message) {
+		this.code = code;
+		this.message = message;
+	}
 
-		  @Override
-		  public String toString() {
-		    return code + ": " + message;
-		  }
+	public String getDescription() {
+		return message;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+		@Override
+		public String toString() {
+			return code + ": " + message;
+		}
 		  
 		  
 }
