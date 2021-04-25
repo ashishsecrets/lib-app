e6f84ccb979c345f360d55ea9ae78bebdfd5e267
ashishsecrets <ashishsecrets@users.noreply.github.com>
2021-04-25 14:25:12 +0530

    Fixed an issue associated with entering numbers separated by comma in answers

--- src/main/java/com/ucsf/service/impl/AnswerImpl/ScreeningTest.java
+++ src/main/java/com/ucsf/service/impl/AnswerImpl/ScreeningTest.java
@@ -9,11 +9,13 @@ public class ScreeningTest {
 
     public ScreenTestData screenTest(ScreeningAnswers lastAnswer){
         ScreenTestData screenTestData = new ScreenTestData();
-
-        if(Integer.parseInt(lastAnswer.getAnswerDescription()) < 12 || Integer.parseInt(lastAnswer.getAnswerDescription()) > 65){
-           screenTestData.setIsFinished(true);
-           screenTestData.setMessage("Sorry, this study is only for individuals between the ages of 12 and 65!");
+    if (lastAnswer.getIndexValue()==1) {
+        if (Integer.parseInt(lastAnswer.getAnswerDescription()) < 12 || Integer.parseInt(lastAnswer.getAnswerDescription()) > 65) {
+            screenTestData.setIsFinished(true);
+            screenTestData.setMessage("Sorry, this study is only for individuals between the ages of 12 and 65!");
         }
+
+    }
         else{
             screenTestData.setIsFinished(false);
             screenTestData.setMessage("continue");
