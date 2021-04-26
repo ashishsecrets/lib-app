7b4fbc354c0b96e81dfe27dc3fe8a07c577d8ec6
Gurpreet <gurpreet.kaur@redblink.net>
2021-04-26 13:02:18 +0530

    Resolved conflicts

--- src/main/java/com/ucsf/service/impl/AnswerImpl/ScreeningTest.java
+++ src/main/java/com/ucsf/service/impl/AnswerImpl/ScreeningTest.java
@@ -46,12 +46,12 @@ public class ScreeningTest {
     
      if (lastAnswer.getIndexValue()==6) {
         if (lastAnswer.getAnswerDescription()!= null && lastAnswer.getAnswerDescription().equals("None of Above")) {
-            screenTestData.setMessage("Thank you for your interest! It looks like you do not qualify for this study, but if anything changes in the future please feel free to reach out. - UCSF Psoriasis and Skin Treatment Center");
+            screenTestData.setMessage("Thank you for your interest in this study! We will review your answers and will follow up with you via email or phone to discuss in detail the study and your ability to participate.");
             screenTestData.setIsFinished(true);
         }
         else{
-            screenTestData.setIsFinished(false);
-            screenTestData.setMessage("continue");
+            screenTestData.setMessage("Thank you for your interest! It looks like you do not qualify for this study, but if anything changes in the future please feel free to reach out. - UCSF Psoriasis and Skin Treatment Center");
+            screenTestData.setIsFinished(true);
         }
     }
         return screenTestData;
