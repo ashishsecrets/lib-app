7ba1fd88db5f538e1c699814c694afe7eb2a5744
Arshdeep <arshdeep.kaur@redblink.net>
2021-05-03 15:31:20 +0530

    Added changes for pushNotifications and aws

---
+++ src/main/java/com/ucsf/service/PushNotificationService.java
@@ -0,0 +1,34 @@
+package com.ucsf.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import com.ucsf.payload.request.Note;
+
+@Service
+public class PushNotificationService {
+
+	@Autowired FirebaseMessaging firebaseMessaging;
+
+    public String sendNotification(Note note, String token) throws FirebaseMessagingException {
+
+        Notification notification = Notification
+                .builder()
+                .setTitle(note.getSubject())
+                .setBody(note.getContent())
+                .build();
+
+        Message message = Message
+                .builder()
+                .setToken(token)
+                .setNotification(notification)
+                .putAllData(note.getData())
+                .build();
+
+        return firebaseMessaging.send(message);
+    }
+}
