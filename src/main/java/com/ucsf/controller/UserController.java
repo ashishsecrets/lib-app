effe61dd513e6a5d7a24311f114d6e391288b120
Gurpreet <gurpreet.kaur@redblink.net>
2021-05-18 15:59:36 +0530

    added endpoint for getting user by id

--- src/main/java/com/ucsf/controller/UserController.java
+++ src/main/java/com/ucsf/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.ucsf.controller;
 
 import com.ucsf.job.LoadStudyInformatives;
-import com.ucsf.job.LoadSurveyQuestions;
+import com.ucsf.payload.response.ErrorResponse;
+
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ucsf.auth.model.User;
+import com.ucsf.common.Constants;
+import com.ucsf.common.ErrorCodes;
 import com.ucsf.job.LoadConsentFormData;
 import com.ucsf.job.LoadScreeningQuestions;
 import com.ucsf.service.UserService;
@@ -27,10 +34,10 @@ import io.swagger.annotations.ApiResponses;
 @RequestMapping("/api/users")
 @Api(tags = "User Controller")
 public class UserController {
-	
+
 	@Autowired
 	UserService userService;
-	
+
 	@Autowired
 	LoadScreeningQuestions loadScreeningQuestions;
 
@@ -38,30 +45,53 @@ public class UserController {
 	LoadStudyInformatives loadStudyInformatives;
 
 	@Autowired
-	LoadSurveyQuestions loadSurveyQuestions;
-	
-	@Autowired
 	LoadConsentFormData loadConsentFormData;
-	
+
 	@RequestMapping(value = "/user", method = RequestMethod.GET)
 	public ResponseEntity<?> getAllUsers() throws Exception {
 		loadScreeningQuestions.loadSheetContent();
 		System.out.println("1111111111");
-		//loading informatives
+		// loading informatives
 		loadStudyInformatives.loadSheetContent();
 		loadConsentFormData.loadFormContent();
 		System.out.println("2222222222");
-		loadSurveyQuestions.loadSheetContent();
 		return ResponseEntity.ok("success");
 	}
-	
+
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@ApiOperation(value = "Get all users", notes = "Get all users", code = 200, httpMethod = "GET", produces = "application/json")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "List of all users", response = User.class) })
 	@RequestMapping(value = "/users/{page}/{size}", method = RequestMethod.GET)
 	public Page<User> getUsers(@PathVariable int page, @PathVariable int size) {
-		//Todo get only patients not admin
 		return userService.findAll(page, size);
+	}
+
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@ApiOperation(value = "Get user by id", notes = "Get user by id", code = 200, httpMethod = "GET", produces = "application/json")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "User with Id", response = User.class) })
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getUserById(@PathVariable Long id) {
+		
+		JSONObject response = new JSONObject();
+		User user = null;
+		UserDetails userDetail = (UserDetails) SecurityContextHolder.getContext().getAuthentication()
+				.getPrincipal();
+		if (userDetail != null && userDetail.getUsername() != null) {
+			user = userService.findByEmail(userDetail.getUsername());
+		} else {
+			response.put("error", new ErrorResponse(ErrorCodes.INVALID_AUTHORIZATION_HEADER.code(),
+					Constants.INVALID_AUTHORIZATION_HEADER.errordesc()));
+			return new ResponseEntity(response.toMap(), HttpStatus.UNAUTHORIZED);
+		}
+		try {
+			user = userService.findById(id);
+			response.put("data", user);
+			return new ResponseEntity(response.toMap(), HttpStatus.OK);
+		}
+		catch (Exception e) {
+			response.put("error", e.getMessage());
+			return new ResponseEntity(response.toMap(), HttpStatus.BAD_REQUEST);
+		}
 	}
 
 }
